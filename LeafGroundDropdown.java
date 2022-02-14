21313f89ecd403436fcd0e601a13ba3c9380b37d
AnushaMuthuvelraja <98222036+AnushaMuthuvelraja@users.noreply.github.com>
2022-02-14 23:21:15 +0000

    Update LeafGroundDropdown.java

--- LeafGroundDropdown.java
+++ LeafGroundDropdown.java
@@ -12,38 +12,37 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LeafGroundDropdown {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
-	       ChromeDriver driver= new ChromeDriver();
-	       driver.manage().window().maximize();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.get("http://leafground.com/pages/Dropdown.html");
-		WebElement dropdown1=driver.findElement(By.id("dropdown1"));
-		Select dd=new Select(dropdown1);
-		dd.selectByIndex(4);
-		WebElement dropdown2= driver.findElement(By.name("dropdown2"));
-		Select dd1=new Select(dropdown2);
-		dd1.selectByVisibleText("Selenium");
-		WebElement dropdown3= driver.findElement(By.name("dropdown3"));
-		Select dd2=new Select(dropdown3);
-		dd2.selectByValue("2");
-		WebElement findElement = driver.findElement(By.xpath("(//option[text()='Select your programs']"));
-Select s1=new Select(findElement);
-s1.selectByVisibleText("Appium");
-s1.selectByIndex(2);
-s1.selectByValue("2");
-		
-		
-//driver.findElement(By.xpath("(//button[@id='position']")).getSize());
+		WebElement dropdown1 = driver.findElement(By.xpath("//select[@id='dropdown1']"));
+		Select dIndex = new Select(dropdown1);
+		dIndex.selectByIndex(2);
 
-	
-	
-	
-	
-	
-	
-		
-		
-		
+		WebElement dropdown2 = driver.findElement(By.xpath("//select[@name='dropdown2']"));
+		Select dtext = new Select(dropdown2);
+		dtext.selectByVisibleText("Loadrunner");
+
+		Thread.sleep(2000);
+		WebElement dropdown3 = driver.findElement(By.xpath("//select[@id='dropdown3']"));
+		Select dvalue = new Select(dropdown3);
+		dvalue.selectByValue("2");
+
+		Select ds = new Select(driver.findElement(By.xpath("//select[@class='dropdown']")));
+		List<WebElement> l = ds.getOptions();
+		System.out.println("No of options in Dropdown="+ " " +l.size());
+
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//option[text()='You can also use sendKeys to select']/..")).sendKeys("Appium");
+		driver.findElement(By.xpath("(//option[@value='4'])[6]")).click();
+
 	}
+
+
+
+
 }
+
+
