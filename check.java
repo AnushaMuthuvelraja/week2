73d233735ee6f7143921b217da466386eae57758
AnushaMuthuvelraja <98222036+AnushaMuthuvelraja@users.noreply.github.com>
2022-02-14 23:20:11 +0000

    Update check.java

--- check.java
+++ check.java
@@ -10,7 +10,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class check {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws InterruptedException
 	{
 		// TODO Auto-generated method stub
 
@@ -18,24 +18,38 @@ public class check {
 		ChromeDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("http://leafground.com/pages/checkbox.html");
-		driver.findElement(By.xpath("(//div[@class='example']//input[@type='checkbox'])[5]")).click();
-		System.out.println(driver.findElement(By.xpath("(//div[@class='example']//input[@type='checkbox'])[6]")).isSelected());
-		 WebElement c1 = driver.findElement(By.xpath("(//div[@class='example']//input[@type='checkbox'])[6]"));
-		boolean a=c1.isSelected();
-		 if (a == true)
-		{
-			driver.findElement(By.xpath("(//div[@class='example']//input[@type='checkbox'])[6]")).click();
+		driver.findElement(By.xpath("//input[@type='checkbox'][1]")).click();
+		Thread.sleep(2000);
 
+		WebElement chk = driver.findElement(By.xpath("(//input[@type='checkbox'])[6]"));
+		if (chk.isSelected()) 
+		{ 
+			System.out.println("Is selected");
 		}
-		
-		
-		
-		driver.findElement(By.xpath("(//input[@type='checkbox'])[9]")).isSelected();
+		else 
+		{
+			System.out.println("Not Selected");
+		}
+
+		WebElement deselect = driver.findElement(By.xpath("//*[@id=\"contentblock\"]/section/div[3]/div[1]/input"));
+		if(deselect.isSelected());
+		{
+			deselect.click();
+		}
+		WebElement deselect1 = driver.findElement(By.xpath("//*[@id=\"contentblock\"]/section/div[3]/div[2]/input"));
+		if(deselect1.isSelected());
+		{
+			deselect1.click();
+		}
+		driver.findElement(By.xpath("//*[@id=\"contentblock\"]/section/div[4]/div[1]/input")).click();
+		driver.findElement(By.xpath("//*[@id=\"contentblock\"]/section/div[4]/div[2]/input")).click();
+		driver.findElement(By.xpath("//*[@id=\"contentblock\"]/section/div[4]/div[3]/input")).click();
+		driver.findElement(By.xpath("//*[@id=\"contentblock\"]/section/div[4]/div[4]/input")).click();
+		driver.findElement(By.xpath("//*[@id=\"contentblock\"]/section/div[4]/div[5]/input")).click();
+
 
 	}
 
 }
-	
-		
-		
-		
+
+
